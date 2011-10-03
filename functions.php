b7534cfcfa7322173b93d1921d725ef6a76e0687
Saketh kasibatla <sake.kasi@gmail.com>
2011-10-03 16:14:17 -0700

    fixed comments interface

--- functions.php
+++ functions.php
@@ -135,4 +135,62 @@
 		return str_replace("<a ", '<a class="page_item_link" ', $t);
 	}
 	add_filter('wp_page_menu', 'add_anchorclass');
+	
+	// Custom callback to list comments in the bluengray style
+	function custom_comments($comment, $args, $depth) {
+	  $GLOBALS['comment'] = $comment;
+    	$GLOBALS['comment_depth'] = $depth;
+	  ?>
+    	<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+	        <div class="comment-author vcard"><?php commenter_link() ?></div>
+    	    <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'bluengray'),
+        	            get_comment_date(),
+            	        get_comment_time(),
+                	    '#comment-' . get_comment_ID() );
+                    	edit_comment_link(__('Edit', 'bluengray'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
+	  <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'bluengray') ?>
+    	      <div class="comment-content">
+        	    <?php comment_text() ?>
+	        </div>
+    	    <?php // echo the comment reply link
+        	    if($args['type'] == 'all' || get_comment_type() == 'comment') :
+            	    comment_reply_link(array_merge($args, array(
+                	    'reply_text' => __('Reply','bluengray'),
+                    	'login_text' => __('Log in to reply.','bluengray'),
+	                    'depth' => $depth,
+    	                'before' => '<div class="comment-reply-link">',
+        	            'after' => '</div>'
+            	    )));
+	            endif;
+    	    ?>
+	<?php } // end custom_comments
+	
+	// Custom callback to list pings
+	function custom_pings($comment, $args, $depth) {
+    	   $GLOBALS['comment'] = $comment;
+        	?>
+            	<li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+                	<div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'bluengray'),
+                    	    get_comment_author_link(),
+                        	get_comment_date(),
+	                        get_comment_time() );
+    	                    edit_comment_link(__('Edit', 'bluengray'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
+	    <?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your trackback is awaiting moderation.</span>\n', 'bluengray') ?>
+    	        <div class="comment-content">
+        	        <?php comment_text() ?>
+            	</div>
+	<?php } // end custom_pings
+	
+	// Produces an avatar image with the hCard-compliant photo class
+	function commenter_link() {
+    	$commenter = get_comment_author_link();
+	    if ( ereg( '<a[^>]* class=[^>]+>', $commenter ) ) {
+    	    $commenter = ereg_replace( '(<a[^>]* class=[\'"]?)', '\\1url ' , $commenter );
+	    } else {
+    	    $commenter = ereg_replace( '(<a )/', '\\1class="url "' , $commenter );
+	    }
+    	$avatar_email = get_comment_author_email();
+	    $avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, 80 ) );
+    	echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
+	} // end commenter_link
 ?>
