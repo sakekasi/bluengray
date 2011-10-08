84746a2b015727f1c813071fc842fa3cd3aa6a43
Saketh kasibatla <sake.kasi@gmail.com>
2011-10-08 13:05:29 -0700

    fixed errors with the 404 template

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@
  
                 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 	<div id="post-0" class="post error404 not-found">
-                		<h1 class="entry-title"><?php _e( 'Not Found', 'bluengray' ); ?></h1>
-                		<div class="entry-content">
-                    		<p><?php _e( 'Apologies, but we were unable to find what you were looking for. Perhaps searching will help.', 'bluengray' ); ?></p>
+                		<h1 class="entry-title"><?php _e( 'Error 404:Not Found', 'bluengray' ); ?></h1>
+                		<div class="search-not-found entry-content">
+                    		<p><?php _e( 'Sorry, but the page you were looking for does not exist. Maybe searching will help.', 'bluengray' ); ?></p>
 							<?php get_search_form(); ?>
                 		</div><!-- .entry-content -->
             		</div><!-- #post-0 -->
